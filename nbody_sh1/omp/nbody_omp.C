0ceb7b9325df1c641a571db01c18d70b13b75157
Celene <hsuan_ling@hotmail.com>
2022-12-04 22:24:26 -0500

    - changing omp algorithm, commit to update server code;

--- nbody_sh1/omp/nbody_omp.C
+++ nbody_sh1/omp/nbody_omp.C
@@ -151,22 +151,19 @@ int main(int argc, char *argv[])
     bool  x_flag = false;      // if true: extra debugging diagnostics output
 
     
-    
-    
-    
-    
     if (! read_options(argc, argv, dt_param, dt_dia, dt_out, dt_tot, init_out,
                        x_flag)){
         printf("ERROR: halt criterion detected by read_options()\n");
         return 1;                // halt criterion detected by read_options()
     }
-        
 
     int n;                       // N, number of particles in the N-body system
     cin >> n;
 
     real t;                      // time
     cin >> t;
+    
+    //omp_set_num_threads(n);
 
     real * mass = new real[n];                  // masses for all particles
     real (* pos)[NDIM] = new real[n][NDIM];     // positions for all particles
@@ -367,6 +364,8 @@ void write_diagnostics(const real mass[], const real pos[][NDIM],
  *-----------------------------------------------------------------------------
  */
 
+
+//instead of array mass[], we take the mass of one body to calculate
 void evolve(const real mass[], real pos[][NDIM], real vel[][NDIM],
             int n, real & t, real dt_param, real dt_dia, real dt_out,
             real dt_tot, bool init_out, bool x_flag)
@@ -391,7 +390,7 @@ void evolve(const real mass[], real pos[][NDIM], real vel[][NDIM],
     real (* jerk)[NDIM] = new real[n][NDIM];         // for all particles
     real epot;                        // potential energy of the n-body system
     real coll_time;                   // collision (close encounter) time scale
-
+    //initial diagnostic
     get_acc_jerk_pot_coll(mass, pos, vel, acc, jerk, n, epot, coll_time);
 
     int nsteps = 0;               // number of integration time steps completed
@@ -405,13 +404,17 @@ void evolve(const real mass[], real pos[][NDIM], real vel[][NDIM],
     real t_dia = t + dt_dia;           // next time for diagnostics output
     real t_out = t + dt_out;           // next time for snapshot output
     real t_end = t + dt_tot;           // final time, to finish the integration
-
+    
+    //#pragma omp parallel private(t_dia, t_out, t_end) shared(t, n)
+    //#pragma omp parallel
+    
     while (true){
         while (t < t_dia && t < t_out && t < t_end){
             real dt = dt_param * coll_time;
-            evolve_step(mass, pos, vel, acc, jerk, n, t, dt, epot, coll_time);
+            evolve_step(mass, pos, vel, acc, jerk, n, t, dt, epot, coll_time); //parallel this step
             nsteps++;
         }
+        
         if (t >= t_dia){
             write_diagnostics(mass, pos, vel, acc, jerk, n, t, epot, nsteps,
                               einit, false, x_flag);
@@ -444,22 +447,35 @@ void evolve_step(const real mass[], real pos[][NDIM], real vel[][NDIM],
     real (* old_acc)[NDIM] = new real[n][NDIM];
     real (* old_jerk)[NDIM] = new real[n][NDIM];
     
-    #pragma omp parallel for num_threads(32)
+    //#pragma omp parallel for
     for (int i = 0; i < n ; i++){
-        cerr <<  "openMP =  "
-             << omp_get_num_threads() << endl;
         for (int k = 0; k < NDIM ; k++){
           old_pos[i][k] = pos[i][k];
           old_vel[i][k] = vel[i][k];
           old_acc[i][k] = acc[i][k];
           old_jerk[i][k] = jerk[i][k];
+            
         }
     }
-
+    
+    /*
+    for (int i = 0; i < n ; i++){
+        for (int k = 0; k < NDIM ; k++){
+            predict_step(pos[i][k], velpos[i][k], accpos[i][k], jerkpos[i][k], n, dt);
+            get_acc_jerk_pot_coll(mass[i], pos[i][k], vel[i][k], acc[i][k], jerk[i][k], n, epot, coll_time);
+            correct_step(pos[i][k], vel[i][k], acc[i][k], jerk[i][k], old_pos[i][k], old_vel[i][k], old_acc[i][k], old_jerk[i][k], n, dt);
+        }
+    }
+    */
+    
+    
+    //Parallel this at one single time
     predict_step(pos, vel, acc, jerk, n, dt);
     get_acc_jerk_pot_coll(mass, pos, vel, acc, jerk, n, epot, coll_time);
     correct_step(pos, vel, acc, jerk, old_pos, old_vel, old_acc, old_jerk,
                  n, dt);
+    
+    
     t += dt;
 
     delete[] old_pos;
@@ -475,19 +491,37 @@ void evolve_step(const real mass[], real pos[][NDIM], real vel[][NDIM],
  *-----------------------------------------------------------------------------
  */
 
-void predict_step(real pos[][NDIM], real vel[][NDIM], 
-                  const real acc[][NDIM], const real jerk[][NDIM],
+//void predict_step(real pos[][NDIM], real vel[][NDIM],
+//                  const real acc[][NDIM], const real jerk[][NDIM],
+//                  int n, real dt)
+//{
+    //#pragma omp parallel for shared(dt)
+    //for (int i = 0; i < n ; i++){
+        //cerr <<  "openMP =  " << omp_get_num_threads() << endl;
+        //for (int k = 0; k < NDIM ; k++){
+        //    pos[i][k] += vel[i][k]*dt + acc[i][k]*dt*dt/2
+        //                              + jerk[i][k]*dt*dt*dt/6;
+        //    vel[i][k] += acc[i][k]*dt + jerk[i][k]*dt*dt/2;
+        //}
+    //}
+    
+//}
+
+
+//celene
+void predict_step(real pos, real vel,
+                  const real acc, const real jerk,
                   int n, real dt)
 {
-    #pragma omp parallel for //num_threads(32)
-    for (int i = 0; i < n ; i++){
-        for (int k = 0; k < NDIM ; k++){
-            pos[i][k] += vel[i][k]*dt + acc[i][k]*dt*dt/2
-                                      + jerk[i][k]*dt*dt*dt/6;
-            vel[i][k] += acc[i][k]*dt + jerk[i][k]*dt*dt/2;
-        }
-    }
+    pos += vel*dt + acc*dt*dt/2 + jerk*dt*dt*dt/6;
+    vel += acc*dt + jerk*dt*dt/2;
+    
 }
+
+
+
+
+
 
 /*-----------------------------------------------------------------------------
  *  correct_step  --  takes one iteration to improve the new values of position
@@ -503,7 +537,7 @@ void correct_step(real pos[][NDIM], real vel[][NDIM],
                   const real old_acc[][NDIM], const real old_jerk[][NDIM],
                   int n, real dt)
 {
-    //#pragma omp parallel for
+    #pragma omp parallel for shared(dt)
     for (int i = 0; i < n ; i++)
         for (int k = 0; k < NDIM ; k++){
             vel[i][k] = old_vel[i][k] + (old_acc[i][k] + acc[i][k])*dt/2
@@ -551,12 +585,13 @@ void correct_step(real pos[][NDIM], real vel[][NDIM],
  *-----------------------------------------------------------------------------
  */
 
+/*
 void get_acc_jerk_pot_coll(const real mass[], const real pos[][NDIM],
                            const real vel[][NDIM], real acc[][NDIM],
                            real jerk[][NDIM], int n, real & epot,
                            real & coll_time)
 {
-    #pragma omp parallel for num_threads(32)
+    //#pragma omp parallel for shared(dt)
     for (int i = 0; i < n ; i++)
         for (int k = 0; k < NDIM ; k++)
             acc[i][k] = jerk[i][k] = 0;
@@ -626,6 +661,104 @@ void get_acc_jerk_pot_coll(const real mass[], const real pos[][NDIM],
     }                                               // from q for quartic back
     coll_time = sqrt(sqrt(coll_time_q));            // to linear collision time
 }                                             
+*/
+
+//celene
+void get_acc_jerk_pot_coll(const real mass[], const real pos[][NDIM],
+                           const real vel[][NDIM], real acc[][NDIM],
+                           real jerk[][NDIM], int n, real & epot,
+                           real & coll_time)
+{
+
+    acc = jerk = 0;
+    epot = 0;
+    const real VERY_LARGE_NUMBER = 1e300;
+    real coll_time_q = VERY_LARGE_NUMBER;      // collision time to 4th power
+    real coll_est_q;                           // collision time scale estimate
+                                               // to 4th power (quartic)
+    
+    
+    for (int i = 0; i < n ; i++)
+        for (int k = 0; k < NDIM ; k++)
+            acc[i][k] = jerk[i][k] = 0;
+    epot = 0;
+    const real VERY_LARGE_NUMBER = 1e300;
+    real coll_time_q = VERY_LARGE_NUMBER;      // collision time to 4th power
+    real coll_est_q;                           // collision time scale estimate
+                                               // to 4th power (quartic)
+    
+    //celene: here comes issue
+    
+    
+    for(int j = 0; j < n; j++){
+        if (j != i){
+            
+            
+            
+            
+        }
+    }
+    
+    
+    
+    for (int i = 0; i < n ; i++){
+        for (int j = i+1; j < n ; j++){            // rji[] is the vector from
+            real rji[NDIM];                        // particle i to particle j
+            real vji[NDIM];                        // vji[] = d rji[] / d t
+            for (int k = 0; k < NDIM ; k++){
+                rji[k] = pos[j][k] - pos[i][k];
+                vji[k] = vel[j][k] - vel[i][k];
+            }
+            real r2 = 0;                           // | rji |^2
+            real v2 = 0;                           // | vji |^2
+            real rv_r2 = 0;                        // ( rij . vij ) / | rji |^2
+            for (int k = 0; k < NDIM ; k++){
+                r2 += rji[k] * rji[k];
+                v2 += vji[k] * vji[k];
+                rv_r2 += rji[k] * vji[k];
+            }
+            rv_r2 /= r2;
+            real r = sqrt(r2);                     // | rji |
+            real r3 = r * r2;                      // | rji |^3
+
+            // add the {i,j} contribution to the total potential energy for the system:
+            epot -= mass[i] * mass[j] / r;
+
+            // add the {j (i)} contribution to the {i (j)} values of acceleration and jerk:
+            real da[3];                            // main terms in pairwise
+            real dj[3];                            // acceleration and jerk
+            for (int k = 0; k < NDIM ; k++){
+                da[k] = rji[k] / r3;                           // see equations
+                dj[k] = (vji[k] - 3 * rv_r2 * rji[k]) / r3;    // in the header
+            }
+            for (int k = 0; k < NDIM ; k++){
+                acc[i][k] += mass[j] * da[k];                 // using symmetry
+                acc[j][k] -= mass[i] * da[k];                 // find pairwise
+                jerk[i][k] += mass[j] * dj[k];                // acceleration
+                jerk[j][k] -= mass[i] * dj[k];                // and jerk
+            }
+
+            // first collision time estimate, based on unaccelerated linear motion:
+            coll_est_q = (r2*r2) / (v2*v2);
+            if (coll_time_q > coll_est_q)
+                coll_time_q = coll_est_q;
+
+            // second collision time estimate, based on free fall:
+            real da2 = 0;                                  // da2 becomes the
+            for (int k = 0; k < NDIM ; k++)                // square of the
+                da2 += da[k] * da[k];                      // pair-wise accel-
+            double mij = mass[i] + mass[j];                // eration between
+            da2 *= mij * mij;                              // particles i and j
+
+            coll_est_q = r2/da2;
+            if (coll_time_q > coll_est_q)
+                coll_time_q = coll_est_q;
+        }
+    }                                               // from q for quartic back
+    coll_time = sqrt(sqrt(coll_time_q));            // to linear collision time
+}
+
+
 
 /*-----------------------------------------------------------------------------
  *                                                                    \\   o
